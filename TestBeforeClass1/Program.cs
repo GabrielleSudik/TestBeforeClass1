bc9da2ee83becbed761ab34ff895ffd8bda09c12
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-11-25 11:39:39 -0500

    first commit

---
+++ TestBeforeClass1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //test run before class
+
+            Console.WriteLine("What's your favorite numnber?");
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What's your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Your favorite number is {number}, {name}!");
+
+            Console.ReadLine();
+        }
+    }
+}
